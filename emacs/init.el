aa77cff9ec862fb98ebdcfb46c060006b89aae8d
llamandtea <giuss.lam@protonmail.com>
2022-11-13 11:52:31 +0100

    Update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -8,39 +8,57 @@
    '("998975856274957564b0ab8f4219300bca12a0f553d41c1438bbca065f298a29" default))
  '(inhibit-startup-screen t)
  '(initial-frame-alist '((fullscreen . maximized)))
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 2.0 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(package-selected-packages
-   '(textsize web-mode magit yasnippet-snippets yasnippet eglot docker-compose-mode dockerfile-mode docker smart-mode-line acme-theme dashboard projectile ## company))
+   '(exec-path-from-shell haskell-mode web-mode magit yasnippet-snippets yasnippet eglot docker-compose-mode dockerfile-mode docker smart-mode-line acme-theme dashboard projectile ## company))
  '(visible-bell t)
  '(warning-suppress-types '((comp) (comp))))
 
+
+;; Enable MELPA repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+
+;; Acme theme configuration
+(use-package acme-theme
+  :config
+  (load-theme 'acme t))
+
+
 ;; Set default font
 (when (member "JuliaMono" (font-family-list))
-  (set-frame-font "JuliaMono-16" t t))
+  (setq default-frame-alist '((font . "JuliaMono-16"))))
+
 
 ;; Enable line numbers
 (global-linum-mode 1)
 
+
 ;; Enable column number
 (column-number-mode 1)
 
-;; Disable <C-Z> suspend frame
+
+;; Disable <C-Z> suspend frame and bind to undo
 (global-unset-key (kbd "C-z"))
 (global-set-key "\C-z" 'undo)
+
 
 ;; Enable Ido mode
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 
+
 ;; Enable electric pair
 (electric-pair-mode 1)
+
 
 ;; Set buffer C-x C-b to bs-show
 (global-set-key (kbd "C-x C-b") 'bs-show)
 
-;; Enable MELPA repositories
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;; Org-mode configuration
 (use-package org
@@ -49,17 +67,6 @@
   (setq org-hide-emphasis-markers t)
   (setq org-startup-with-inline-images t))
 
-(use-package textsize
-      :ensure nil
-      :commands textsize-mode
-;;      :config
-;;      (textsize-default-points 16)
-      :init (textsize-mode))
-
-;; Acme theme configuration
-(use-package acme-theme
-  :config
-  (load-theme 'acme t))
 
 ;; Dashboard configuration
 (use-package dashboard
@@ -83,31 +90,34 @@
 ;; Dashboard - Set centered
 (setq dashboard-center-content t)
 
+
 ;; Enable Smart Mode Line
 (sml/setup)
+
 
 ;; Language Server Protocol packages loading
 (use-package eglot
   :ensure t
-  :hook ((python-mode) . eglot-ensure)
-  :hook ((c-mode) . eglot-ensure)
-  :hook ((c++-mode) . eglot-ensure))
+  :hook ((prog-mode) . eglot-ensure))
 
+
+;; Load snippets
 (use-package yasnippet
   :ensure t
-  :hook (python-mode . (lambda ()
+  :hook (prog-mode . (lambda ()
 			 (yas-reload-all)
 			 (yas-minor-mode))))
 
+
+;; Autocompletion
 (use-package company
-  :hook ((python-mode) . company-mode)
-  :hook ((c-mode) . company-mode)
-  :hook ((c++-mode) . company-mode))
+  :hook prog-mode)
+
 
 ;; Docker modes
 (use-package dockerfile-mode)
-
 (use-package docker-compose-mode)
+
 
 ;; Projectile configuration
 (projectile-mode +1)
@@ -115,11 +125,14 @@
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-indexing-method 'hybrid)
 
+
 ;; Initialize MELPA packages
 (package-initialize)
 
-;; MELPA automatic configurations
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
+;; MELPA automatic configurations
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
